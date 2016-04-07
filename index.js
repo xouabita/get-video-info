43978eb1724fb699a8e82e44045635e732806669
Alexandre Abita <abita_a@epita.fr>
2016-04-07 18:34:44 +0200

    use ffprobe-static

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const exec   = require('mz/child_process').exec
-const co     = require('co')
-const exists = require('path-exists')
+const exec    = require('mz/child_process').exec
+const co      = require('co')
+const exists  = require('path-exists')
+const ffprobe = require('ffprobe-static').path
 
 function getVideoInfo (video) {
   return co(function* () {
     if (!(yield exists(video)))
       throw new Error(`${video} is not a valid path`)
 
-    var out = yield exec('ffprobe -v quiet -print_format json -show_format -show_streams ' + video)
+    var out = yield exec(`${ffprobe} -v quiet -print_format json -show_format -show_streams ${video}`)
     return JSON.parse(out[0])
   })
 }
